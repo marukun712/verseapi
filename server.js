17be54999127ff5b1889eb634a9421df1c73c5e5
marukun_ <87289638+marukun712@users.noreply.github.com>
2022-04-12 08:54:51 +0900

    cros2

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 import express from "express";
 var app = express();
 var port = 2100
-import cros from 'cros'
 import dotenv from 'dotenv'
 dotenv.config();
 import fs from 'fs'
-app.use(cors())
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization, access_token'
+    )
+
+    // intercept OPTIONS method
+    if ('OPTIONS' === req.method) {
+        res.send(200)
+    } else {
+        next()
+    }
+}
+app.use(allowCrossDomain)
 
 app.get('/api', function(req, res) {
     const json = JSON.parse(fs.readFileSync('./res.json', 'utf8'));
